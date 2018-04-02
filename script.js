cedd0d1f51ff95f680c2d896735131fba2cfc6e1
Glitch (deep-mimosa) <none>
2018-04-02 23:59:50 +0000

    🎬🛎 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('hi');
+
+function MariettaChosen () {
+  var JimImage = document.getElementById("Jim");
+  JimImage.src = "";
+  var AshImage = document.getElementById("Ash");
+  AshImage.src = "";
+}
